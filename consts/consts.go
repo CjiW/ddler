b3598ff38abc8fcb93ea6b9ef8965c29fd0351fa
cjw <64598696+cao-jing-wei@users.noreply.github.com>
2021-11-04 14:50:25 +0800

    Create consts.go

---
+++ consts/consts.go
@@ -0,0 +1,15 @@
+package consts
+
+var (
+	APPID           = "cli_a1f9eeff36f8d00d"
+	APPSECRET       = "NYUvRnqVGfTwKeadurXzXb7salWYMOi1"
+	CHATID          = ""
+	FEISHUOPENURLv1 = "https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=open_id"
+	FEISHUOPENURLv4 = "https://open.feishu.cn/open-apis/message/v4/send/"
+	DETAILsURL      = "https://www.baidu.com/"
+	LISTURL         = "https://www.baidu.com/"
+	DATABASE1       = "root:020103@tcp(127.0.0.1:3306)/tasks?parseTime=true&loc=Local&charset=utf8mb4"
+	DATABASE2       = "root:020103@tcp(127.0.0.1:3306)/users?parseTime=true&loc=Local"
+
+ 	AUTOTIME        = "0 9,21 * * *"
+)
