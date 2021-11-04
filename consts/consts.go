42a74e5f5df96cae7d020fe87e3105194de2dc20
cjw <64598696+cao-jing-wei@users.noreply.github.com>
2021-11-04 15:13:22 +0800

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -1,15 +1,15 @@
 package consts
 
 var (
-	APPID           = "cli_a1f9eeff36f8d00d"
-	APPSECRET       = "NYUvRnqVGfTwKeadurXzXb7salWYMOi1"
-	CHATID          = ""
+	APPID           = "cli_xxxx"
+	APPSECRET       = "xxxx"
+	CHATID          = "xxxx"
 	FEISHUOPENURLv1 = "https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=open_id"
 	FEISHUOPENURLv4 = "https://open.feishu.cn/open-apis/message/v4/send/"
 	DETAILsURL      = "https://www.baidu.com/"
 	LISTURL         = "https://www.baidu.com/"
-	DATABASE1       = "root:020103@tcp(127.0.0.1:3306)/tasks?parseTime=true&loc=Local&charset=utf8mb4"
-	DATABASE2       = "root:020103@tcp(127.0.0.1:3306)/users?parseTime=true&loc=Local"
+	DATABASE1       = "root:xxx@tcp(127.0.0.1:3306)/tasks?parseTime=true&loc=Local&charset=utf8mb4"
+	DATABASE2       = "root:xxx@tcp(127.0.0.1:3306)/users?parseTime=true&loc=Local"
 
  	AUTOTIME        = "0 9,21 * * *"
 )
