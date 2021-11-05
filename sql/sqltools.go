40cabdb8ae1a56b5dbc8f8ff06a6bdfc6837a546
cjw <64598696+cao-jing-wei@users.noreply.github.com>
2021-11-05 15:09:13 +0800

    update

--- sql/sqltools.go
+++ sql/sqltools.go
@@ -2,46 +2,52 @@ package sql
 
 import (
 	"errors"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 	"main/consts"
 	"main/tools"
 	"time"
+
+	"gorm.io/gorm"
 )
 
-func UpdateUserlist() {
-	db, _ := gorm.Open(mysql.Open(consts.DATABASE2), &gorm.Config{})
-	_ = db.AutoMigrate(&tools.User{})
-	openids,names :=tools.GetMember(consts.CHATID)
+func UpdateUserlist(chatid string) {
+
+	openids,names :=tools.GetMember(chatid)
 	for i := 0; i < len(openids); i++ {
 		var user tools.User
-		errNameIsNotExisting :=db.First(&user,"openid=?",openids[i]).Error
+		errNameIsNotExisting :=consts.GlobalDB.First(&user,"open_id=?",openids[i]).Error
 		if errors.Is(errNameIsNotExisting, gorm.ErrRecordNotFound){
-		db.Create(&tools.User{Name:names[i],OpenId: openids[i]})
+		consts.GlobalDB.Create(&tools.User{Name:names[i],OpenId: openids[i]})
 	}else {
 		user.Name = names[i]
-		db.Save(&user)
+		consts.GlobalDB.Save(&user)
 	}
 	}
 }
-func GetUserInf(username string) tools.User {
+func GetUserInf1(username string) tools.User {
 	var user tools.User
-	db, _ := gorm.Open(mysql.Open(consts.DATABASE2), &gorm.Config{})
-	errNameIsNotExisting :=db.First(&user,"name=?",username).Error
+	errNameIsNotExisting :=consts.GlobalDB.First(&user,"name=?",username).Error
 	if errors.Is(errNameIsNotExisting, gorm.ErrRecordNotFound){
 		return tools.User{Name: username,OpenId: ""}
 	}else {return user}
 
 }
+func GetUserInf2(userid string) tools.User {
+	var user tools.User
+	errNameIsNotExisting :=consts.GlobalDB.First(&user,"open_id=?",userid).Error
+	if errors.Is(errNameIsNotExisting, gorm.ErrRecordNotFound){
+		return tools.User{Name: "",OpenId: userid}
+	}else {return user}
+
+}
+
 
 //AddTask 创建任务
 func AddTask(task tools.Task) error {
-	db, _ := gorm.Open(mysql.Open(consts.DATABASE1), &gorm.Config{})
-	_ = db.AutoMigrate(&tools.TaskData{})
-	errNameIsNotExisting :=db.First(&tools.TaskData{},"name=?",task.Name).Error
+	_ = consts.GlobalDB.AutoMigrate(&tools.TaskData{})
+	errNameIsNotExisting :=consts.GlobalDB.First(&tools.TaskData{},"name=?",task.Name).Error
 	if errors.Is(errNameIsNotExisting, gorm.ErrRecordNotFound){
         taskdata := task.TurnIn()
-        _ = db.Create(&taskdata)
+        _ = consts.GlobalDB.Create(&taskdata)
 		tools.SendMsg("任务发布成功:"+task.Name,"text",[]string{task.SenderId})
 		return nil
 	}else {
@@ -55,8 +61,7 @@ func GetTaskInf(taskName string) tools.Task {
 	var taskdata tools.TaskData
 	var task tools.Task
 
-	db, _ := gorm.Open(mysql.Open(consts.DATABASE1), &gorm.Config{})
-	errNameIsNotExisting :=db.First(&taskdata,"name=?",taskName).Error
+	errNameIsNotExisting :=consts.GlobalDB.First(&taskdata,"name=?",taskName).Error
 	if errors.Is(errNameIsNotExisting, gorm.ErrRecordNotFound){return tools.Task{}}
 	task = taskdata.TurnOut()
 	return task
@@ -68,43 +73,44 @@ func FindTask(taskId uint) (tools.Task, error) {
 	var taskdata tools.TaskData
 	var task tools.Task
 
-	db, _ := gorm.Open(mysql.Open(consts.DATABASE1), &gorm.Config{})
-	errNameIsNotExisting :=db.First(&taskdata,"id=?", taskId).Error
+	errNameIsNotExisting :=consts.GlobalDB.First(&taskdata,"id=?", taskId).Error
 	if errors.Is(errNameIsNotExisting, gorm.ErrRecordNotFound){return tools.Task{}, errors.New("name is not existing")}
 	task = taskdata.TurnOut()
 	return task, nil
 }
 
 //FinishTask 执行完成任务后的状态修改及提醒
-func FinishTask(taskName, doneId string) tools.Task {
+func FinishTask(taskName, doneId string) (tools.Task,string) {
 	var (
 		taskdata tools.TaskData
 		task tools.Task
+		msg string
 	)
-	db, _ := gorm.Open(mysql.Open(consts.DATABASE1), &gorm.Config{})
-	errNameIsNotExisting :=db.First(&taskdata,"name=?",taskName).Error
+	errNameIsNotExisting :=consts.GlobalDB.First(&taskdata,"name=?",taskName).Error
 	if errors.Is(errNameIsNotExisting, gorm.ErrRecordNotFound){
-
+		msg="未找到任务："+taskName
 	}else {
 		task = taskdata.TurnOut()
+		msg = "您不在未完成列表中"
 		for i :=0;i<len(task.UndoneId);i++ {
 			if task.UndoneId[i]==doneId{
-				task.UndoneId = append(task.UndoneId[:i], task.UndoneId[i+1:]...)}
+				task.UndoneId = append(task.UndoneId[:i], task.UndoneId[i+1:]...)
+				task.DoneId = append(task.DoneId, doneId)
+				msg = "已完成："+taskName}
 		}
-		task.DoneId = append(task.DoneId, doneId)
+		
 		if len(task.UndoneId)==0{task.Status = 1}
 		taskdata = task.TurnIn()
-        db.Save(&taskdata)
+        consts.GlobalDB.Save(&taskdata)
 	}
-	return task
+	return task, msg
 }
 
 //AutoRemind 遍历任务，自动提醒
 func AutoRemind() {
 	var taskdatas []tools.TaskData
 	var tasks []tools.Task
-	db, _ := gorm.Open(mysql.Open(consts.DATABASE1), &gorm.Config{})
-	db.Find(&taskdatas)
+	consts.GlobalDB.Find(&taskdatas)
 	for i:=0;i<len(taskdatas);i++{
 		tasks = append(tasks, taskdatas[i].TurnOut())
 	}
@@ -125,16 +131,16 @@ func AutoRemind() {
 }}
 func Remind(taskId uint) error{
 	task,err := FindTask(taskId)
+	if task.Status == 0 {
 	tools.SendMsg(tools.RemindMsg(task.SenderId,task.Name,task.End.Format("2006/01/02"),int(task.End.Sub(time.Now()).Hours()/24)),
 		"card",
-		task.UndoneId)
+		task.UndoneId)}
 	return err
 }
 
 func ChangeTask(json2 tools.H5json2) error {
 	var taskdata tools.TaskData
-	db, _ := gorm.Open(mysql.Open(consts.DATABASE1), &gorm.Config{})
-	errIdIsNotExisting :=db.First(&taskdata,"id=?",json2.Taskid).Error
+	errIdIsNotExisting :=consts.GlobalDB.First(&taskdata,"id=?",json2.Taskid).Error
 	if errors.Is(errIdIsNotExisting, gorm.ErrRecordNotFound){return errIdIsNotExisting}
 	start,_ := time.Parse("2006/01/02",json2.Start)
 	end,_ := time.Parse("2006/01/02",json2.Ddl)
@@ -148,7 +154,7 @@ func ChangeTask(json2 tools.H5json2) error {
 		Id:       	json2.Taskid,
 		Name:     	json2.Taskname,
 		Taskcontent:json2.TaskContent,
-		SenderId:	GetUserInf(json2.Sender).OpenId,
+		SenderId:	GetUserInf1(json2.Sender).OpenId,
 		UndoneId: 	undoneids,
 		DoneId:   	doneids,
 		Start:    	start,
@@ -156,6 +162,6 @@ func ChangeTask(json2 tools.H5json2) error {
 		Status:   	json2.Isdone,
 	}
 	taskdata = task.TurnIn()
-	db.Save(taskdata)
+	consts.GlobalDB.Save(taskdata)
 	return nil
 }
