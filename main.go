c81ea621440a9c5f637681cb1ab3933e10acd0dd
cjw <64598696+cao-jing-wei@users.noreply.github.com>
2021-11-05 15:04:34 +0800

    update

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/robfig/cron/v3"
 	"main/consts"
 	"main/router"
 	"main/sql"
+	"main/tools"
+
+	"github.com/gin-gonic/gin"
+	"github.com/robfig/cron/v3"
 )
 
 func main() {
+	
+	tools.InitDB()
 	c := cron.New()
 	_, _ = c.AddFunc(consts.AUTOTIME, sql.AutoRemind)
 	c.Start()
 	r := gin.Default()
+	r.Use(tools.Cors())
 	router.UseRouter(r)
+	
 	_ = r.Run(":10086")
 	//tools.SendMsg(tools.RemindMsg("ou_14661677e00831a1b66d17f03e188706","ddler","2021/10/31",2),"card",[]string{"ou_14661677e00831a1b66d17f03e188706"})
 }
